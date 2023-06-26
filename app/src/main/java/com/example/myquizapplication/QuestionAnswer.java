be89398d135af64dbea009023e94047c2f76f533
Sachin <sachinbareth099@gmail.com>
2023-06-26 11:30:41 +0530

    Final Commit

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,32 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+    public static String[] question = {
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which data type is used to create a variable that should store text?",
+            "Which company owns the apple?",
+            "Which keyword is used to create a class in Java?"
+
+
+    };
+
+    public static String[][] choices = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Txt","string","myString","String"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"className","class","class()","MyClass"}
+
+
+    };
+
+    public static String[] correctAnswers = {
+            "Google",
+            "Notepad",
+            "String",
+            "Apple",
+            "class"
+
+    };
+}
